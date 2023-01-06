56d20d6e0a4185b23e1012d45b52b3db3fb148e3
Victor Hugo <victordovalle88@gmail.com>
2023-01-06 16:13:23 -0300

    apiREST Node com JWT

---
+++ index.js
@@ -0,0 +1,189 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+const JWTSecret = "dasdasdsadasdasdsaadasdas";
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function auth(req, res, next) {
+  const authToken = req.headers["authorization"];
+
+  if (authToken != undefined) {
+    const bearer = authToken.split(" ");
+
+    var token = bearer[1];
+
+    jwt.verify(token, JWTSecret, (err, data) => {
+      if (err) {
+        res.status(401);
+        res.json({ err: "Token invalido" });
+      } else {
+        req.token = token;
+        req.loggedUser = {
+          id: data.id,
+          email: data.email,
+        };
+        console.log(data);
+      }
+    });
+  } else {
+    res.status(401);
+    res.json({ err: "Token invalido" });
+  }
+
+  next();
+}
+
+var FAKEDB = {
+  games: [
+    {
+      id: 23,
+      title: "Call of Duty MW",
+      year: 2019,
+      price: 60,
+    },
+
+    {
+      id: 65,
+      title: "Sea of Thieves",
+      year: 2018,
+      price: 40,
+    },
+    {
+      id: 2,
+      title: "Minecraft",
+      year: 2019,
+      price: 20,
+    },
+  ],
+  users: [
+    {
+      id: 1,
+      name: "User Example",
+      email: "user@example.com",
+      pass: 1234,
+    },
+
+    {
+      id: 2,
+      name: "User Example2",
+      email: "user@example2.com",
+      pass: 5678,
+    },
+  ],
+};
+
+app.get("/games", auth, (req, res) => {
+  res.statusCode = 200;
+  res.json({ user: req.loggedUser, games: FAKEDB.games });
+});
+
+app.get("/game/:id", auth, (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var game = FAKEDB.games.find((g) => g.id == id);
+
+    if (game != undefined) {
+      res.statusCode = 200;
+      res.json(game);
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
+
+app.post("/game", auth, (req, res) => {
+  var { title, price, year } = req.body;
+  FAKEDB.games.push({ id: 34, title, price, year });
+  res.sendStatus(200);
+});
+
+app.put("/game/:id", auth, (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var game = FAKEDB.games.find((g) => g.id == id);
+
+    if (game != undefined) {
+      var { title, price, year } = req.body;
+      if (title != undefined) {
+        game.title = title;
+      }
+      if (price != undefined) {
+        game.price = price;
+      }
+      if (year != undefined) {
+        game.year = year;
+      }
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
+
+app.delete("/game/:id", auth, (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var index = FAKEDB.games.findIndex((g) => g.id == id);
+
+    if (index == -1) {
+      res.sendStatus(404);
+    } else {
+      FAKEDB.games.splice(index, 1);
+      res.sendStatus(200);
+    }
+  }
+});
+
+app.post("/auth", (req, res) => {
+  var { email, pass } = req.body;
+  if (email != undefined) {
+    var user = FAKEDB.users.find((u) => u.email == email);
+    if (user != undefined) {
+      if (user.pass == pass) {
+        jwt.sign(
+          { id: user.id, email: user.email },
+          JWTSecret,
+          {
+            expiresIn: "48h",
+          },
+          (err, token) => {
+            if (err) {
+              res.status(400);
+              res.json({ err: "Falha interna" });
+            } else {
+              res.status(200);
+              res.json({ token: token });
+            }
+          }
+        );
+      } else {
+        res.status(401);
+        res.json({ err: "Credenciais invalidas" });
+      }
+    } else {
+      res.status(404);
+      res.json({ err: "Email inexistente" });
+    }
+  } else {
+    res.status(400);
+    res.json({ err: "Email invalido!" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("API rodando");
+});
