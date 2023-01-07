c909323fa217c4aced1b2caf9d78c171aac7c6c7
Victor Hugo <victordovalle88@gmail.com>
2023-01-07 10:11:57 -0300

    HATEOAS

--- index.js
+++ index.js
@@ -75,8 +75,26 @@ var FAKEDB = {
 };
 
 app.get("/games", auth, (req, res) => {
+  var HATEOAS = [
+    {
+      href: "http://localhost:3000/game/0",
+      method: "GET",
+      rel: "get_game",
+    },
+    {
+      href: "http://localhost:3000/game/0",
+      method: "DELETE",
+      rel: "delete_game",
+    },
+    {
+      href: "http://localhost:3000/auth",
+      method: "POST",
+      rel: "login",
+    },
+  ];
+
   res.statusCode = 200;
-  res.json(FAKEDB.games);
+  res.json({ games: FAKEDB.games, _links: HATEOAS });
 });
 
 app.get("/game/:id", auth, (req, res) => {
@@ -84,11 +102,34 @@ app.get("/game/:id", auth, (req, res) => {
     res.sendStatus(400);
   } else {
     var id = parseInt(req.params.id);
+
+    var HATEOAS = [
+      {
+        href: `http://localhost:3000/game/${id}`,
+        method: "GET",
+        rel: "get_game",
+      },
+      {
+        href: `http://localhost:3000/game/${id}`,
+        method: "DELETE",
+        rel: "delete_game",
+      },
+      {
+        href: `http://localhost:3000/game/${id}`,
+        method: "PUT",
+        rel: "edit_game",
+      },
+      {
+        href: "http://localhost:3000/games",
+        method: "GET",
+        rel: "get_all_games",
+      },
+    ];
     var game = FAKEDB.games.find((g) => g.id == id);
 
     if (game != undefined) {
       res.statusCode = 200;
-      res.json(game);
+      res.json({ game, _links: HATEOAS });
     } else {
       res.sendStatus(404);
     }
